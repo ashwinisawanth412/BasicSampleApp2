8a7e644c09afb88273eb4e5f16e64104d47ed415
kesh-1139 <keshava@dabkick.com>
2018-12-19 13:39:10 +0530

    removed unwanted lines

--- app/src/main/java/dabkick/com/basicsampleapp/HomePageActivity.java
+++ app/src/main/java/dabkick/com/basicsampleapp/HomePageActivity.java
@@ -185,8 +185,7 @@ public class HomePageActivity extends BaseActivity {
                         mRoomListAdapter.enterRoomOnCreation(roomName);
                     } else {
                         mRoomListAdapter = new RoomListAdapter(mRoomList, HomePageActivity.this);
-                        mRoomListView.setAdapter(mRoomListAdapter);
-                        mRoomListView.setLayoutManager(new LinearLayoutManager(HomePageActivity.this));
+                        mRoomListAdapter.enterRoomOnCreation(roomName);
                     }
                 }
                 roomNameEditText.setText("");
